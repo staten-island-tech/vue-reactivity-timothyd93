a888795e4c838efd75f5e42833a0340a22accaea
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2023-03-28 13:55:45 -0400

    a

--- vue-project/src/components/products.vue
+++ vue-project/src/components/products.vue
@@ -1,7 +1,7 @@
 <script setup>
 const products = [
-  { name: "apple", price: 2.0, img: `C:\Users\timothy.deng\Documents\GitHub\vue-reactivity-timothyd93\vue-project\src\components\apple.jpg`},
-  { name: "orange", price: 3.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh24pRhiUoM5TIn9K7S86c_uHhkjBUZuD9Jg&usqp=CAU" },
+  { name: "apple", price: 2.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn4jedgV_XfyLOBuegpPVJQRtwFViVI7dDWw&usqp=CAU"},
+  { name: "orange", price: 3.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYsNDHoyYYbfJPLaz9lRPuMEoMQUsR3JdN2A&usqp=CAU" },
   { name: "pineapple", price: 3.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3jO9eMKEjXv0TQ1S736uUADRPnuu8mKyiVw&usqp=CAU" },
   { name: "grapes", price: 1.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8gU04X6jQNJ_BbTgvAGdoUYPEsg9ok8sS7w&usqp=CAU" },
   { name: "watermelon", price: 4.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAmbgB21P-h8gMIOtE3wXUjMaHRYfsfEJ-cA&usqp=CAU" },
@@ -10,8 +10,8 @@ const products = [
 </script>
 
 <template>
-  <div class="home"  v-for="product in products">
-    <img src={{product.img}}>
-  <li> {{ product.name }}</li>
+  <div class="home">
+    <img v-for="product in products" :src="product.img">
+  <li  v-for="product in products"> {{ product.name }}</li>
   </div>
 </template>
