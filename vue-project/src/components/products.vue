2788380db37a356674fbb7e1e875b116f4060bc3
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2023-04-03 00:09:10 -0400

    A

--- vue-project/src/components/products.vue
+++ vue-project/src/components/products.vue
@@ -1,36 +1,19 @@
-<script setup>
-const products = [
-  { name: "apple", price: 2.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn4jedgV_XfyLOBuegpPVJQRtwFViVI7dDWw&usqp=CAU"},
-  { name: "orange", price: 3.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYsNDHoyYYbfJPLaz9lRPuMEoMQUsR3JdN2A&usqp=CAU" },
-  { name: "pineapple", price: 3.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3jO9eMKEjXv0TQ1S736uUADRPnuu8mKyiVw&usqp=CAU" },
-  { name: "grapes", price: 1.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8gU04X6jQNJ_BbTgvAGdoUYPEsg9ok8sS7w&usqp=CAU" },
-  { name: "watermelon", price: 4.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAmbgB21P-h8gMIOtE3wXUjMaHRYfsfEJ-cA&usqp=CAU" },
-  { name: "pear", price: 2.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTapfbRplXGkS0_olxze9Qs2ExfGlLdzYvGMQ&usqp=CAU" },
-]
-</script>
 <template>
-    <div id="app">
-    <div v-for="product in products" class="cards">
-      <img :src="product.img" class="img" />
-      <p>{{ product.name }}</p>
-      <button>${{ product.price }}.00</button>
+    <div class="cards">
+      <img :src="img" class="img" />
+      <p>{{ name }}</p>
+      <p> ${{ price }}.00</p>
     </div>
-  </div>
   </template>
   
-  <style>
-  #app{ 
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
-  }
-  .cards {
-    text-align: center;
-    height: 500px;
-    width: 500px;
-  }
-  .img{
-    width: 300px;
-    height: 300px;
-  }
-  </style>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    name: String,
+    price: Number,
+    img: String,
+  },
+}
+</script>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,11 +1,82 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-import products from '../components/products.vue'
-</script>
-
 <template>
   <main>
-    <!-- <TheWelcome /> -->
-    <products />
+    <h1 id="header">Fruit Store Online Shopping Cart</h1>
+    <h2 id="total">Total: ${{ total }}</h2>
+    <div id="container">
+    <div v-for="fruit in fruits">
+        <Card
+      :key ="fruit.name"
+      :name ="fruit.name"
+      :price ="fruit.price"
+      :img = "fruit.img"
+      />
+      <button id="button" v-on:click="addPrice(fruit.price)"> Add to Cart</button>
+    </div>
+  </div>  
   </main>
 </template>
+
+<script>
+import Card from "../components/products.vue"
+export default {
+  name: "array",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      fruits: [
+      { name: "Apple", price: 2.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn4jedgV_XfyLOBuegpPVJQRtwFViVI7dDWw&usqp=CAU"},
+    { name: "Orange", price: 3.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYsNDHoyYYbfJPLaz9lRPuMEoMQUsR3JdN2A&usqp=CAU" },
+    { name: "Pineapple", price: 3.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3jO9eMKEjXv0TQ1S736uUADRPnuu8mKyiVw&usqp=CAU" },
+    { name: "Grapes", price: 1.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8gU04X6jQNJ_BbTgvAGdoUYPEsg9ok8sS7w&usqp=CAU" },
+    { name: "Watermelon", price: 4.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAmbgB21P-h8gMIOtE3wXUjMaHRYfsfEJ-cA&usqp=CAU" },
+    { name: "Pear", price: 2.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTapfbRplXGkS0_olxze9Qs2ExfGlLdzYvGMQ&usqp=CAU" },
+      ],
+      total: 0
+    };
+  },
+  methods: {
+    addPrice(fruits) {
+    this.total += fruits
+    console.log(this.total)
+    }
+  },
+};
+</script>
+
+<style>
+#total {
+  text-align: center;
+}
+
+#header{
+  text-align: center;
+  margin-bottom: 4rem;
+}
+
+#button{
+  margin-left: ;
+}
+
+#container{ 
+display: flex;
+flex-wrap: wrap;
+justify-content: space-evenly;
+text-align: center;
+}
+.cards {
+  text-align: center;
+  height: 500px;
+  width: 500px;
+  margin-top: 1rem;
+}
+.img{
+  width: 300px;
+  height: 300px;
+}
+
+#body{
+  background-color: #FAF9F6;
+}
+</style>
