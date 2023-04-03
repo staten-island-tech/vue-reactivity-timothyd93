807238aba104d6150558267a30f5c4318c81a5cf
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2023-04-02 21:39:49 -0400

    a

--- vue-project/src/components/products.vue
+++ vue-project/src/components/products.vue
@@ -5,27 +5,32 @@ const products = [
   { name: "pineapple", price: 3.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3jO9eMKEjXv0TQ1S736uUADRPnuu8mKyiVw&usqp=CAU" },
   { name: "grapes", price: 1.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8gU04X6jQNJ_BbTgvAGdoUYPEsg9ok8sS7w&usqp=CAU" },
   { name: "watermelon", price: 4.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAmbgB21P-h8gMIOtE3wXUjMaHRYfsfEJ-cA&usqp=CAU" },
-  { name: "pear", price: 2.5, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTapfbRplXGkS0_olxze9Qs2ExfGlLdzYvGMQ&usqp=CAU" },
+  { name: "pear", price: 2.0, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTapfbRplXGkS0_olxze9Qs2ExfGlLdzYvGMQ&usqp=CAU" },
 ]
-// const products = ref([
-//   {
-//     name: 'apple',
-//     price: 2.0,
-//     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn4jedgV_XfyLOBuegpPVJQRtwFViVI7dDWw&usqp=CAU'
-//   },
-//   {
-//     name: 'orange',
-//     price: 3.0,  
-//     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYsNDHoyYYbfJPLaz9lRPuMEoMQUsR3JdN2A&usqp=CAU'
-//   }
-// ])
 </script>
-
 <template>
-  <p v-text="x"></p>
-  <div v-for="product in products" class="cards">
-    <img :src="product.img" />
-    <p>{{ product.name }}</p>
-    <button>{{ product.price }}</button>
+    <div id="app">
+    <div v-for="product in products" class="cards">
+      <img :src="product.img" class="img" />
+      <p>{{ product.name }}</p>
+      <button>${{ product.price }}.00</button>
+    </div>
   </div>
-</template>
+  </template>
+  
+  <style>
+  #app{ 
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
+  .cards {
+    text-align: center;
+    height: 500px;
+    width: 500px;
+  }
+  .img{
+    width: 300px;
+    height: 300px;
+  }
+  </style>
